2857e76f9ba4980e17e49fe595fa23a688e392d2
ankihg <hagelin.annika@gmail.com>
2018-02-09 18:08:33 -0800

    train classifier attmpt

---
+++ index.js
@@ -0,0 +1,52 @@
+const natural = require('natural');
+
+const N = 1;
+const START = '<start>';
+const END = '<end>';
+
+const classes = {
+    A: ['plz respond. i love butterflies.', ],
+    B: ['happy cat butt dump. help me.', ],
+};
+const numDocs = classes.A.length + classes.B.length;
+let unitedDoc = Object.keys(classes).reduce((acc, aClass) => {
+    return acc + classes[aClass].reduce((acc, doc) => acc + doc, '') + ' ';
+}, '');
+const vocabulary = natural.NGrams.ngrams(unitedDoc, N);
+console.log(vocabulary);
+const wordCounts = vocabulary.reduce((acc, word) => {
+    acc[word] = (acc[word] || 0) + 1;
+    return acc;
+}, {});
+console.log(wordCounts);
+
+// let ngrams = natural.NGrams.ngrams(input, N, START, END);
+// console.log(ngrams);
+
+const statsByClass = Object.keys(classes).reduce((acc, aClass) => {
+    let numDocsOfClass = classes[aClass].length;
+    acc.logprior[aClass] = Math.log(numDocsOfClass / numDocs);
+
+    let unitedDocOfClass = classes[aClass].reduce((acc, doc) => acc + doc, '');
+    let wordCountsOfClass = natural.NGrams.ngrams(unitedDocOfClass, N).reduce((acc, word) => {
+        acc[word] = (acc[word] || 0) + 1;
+        return acc;
+    }, {});
+
+    acc.loglikelihood[aClass] = Object.keys(wordCounts).reduce((acc, word) => {
+        console.log('num', ((wordCountsOfClass[word] || 0) + 1));
+        console.log('dnm', Object.keys(wordCounts).reduce((sum, _word) => sum + (wordCountsOfClass[_word] || 0) + 1 , 0));
+        acc[word] = //Math.log(
+            ((wordCountsOfClass[word] || 0) + 1) /
+            Object.keys(wordCounts).reduce((sum, _word) => sum + (wordCountsOfClass[_word] || 0) + 1 , 0)
+         );
+         return acc;
+    }, {});
+
+    return acc;
+}, {
+    logprior: {},
+    loglikelihood: {},
+});
+
+console.log(statsByClass);
